e5aa721731e9b86e502b3b239379205cd2590429
amejid <abdelmejid89@gmail.com>
2023-01-15 17:38:52 +0300

    Search with ransack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_notifications, if: :current_user
+  before_action :set_query
 
   private
 
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
     notifications = Notification.where(recipient: current_user).newest_first.limit(9)
     @unread = notifications.unread
     @read = notifications.read
+  end
+
+  def set_query
+    @query = Post.ransack(params[:q])
   end
 end
