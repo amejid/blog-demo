dde5ab33c4d0cfcaf21680e96272dc83b5d97d21
amejid <abdelmejid89@gmail.com>
2023-01-15 14:27:23 +0300

    Update comment with stimulus

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: "mail@mail.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Amejid", email: "mail@mail.com", password: "123456", password_confirmation: "123456")
+User.create(name: "John", email: "test@mail.com", password: "123456", password_confirmation: "123456")
 
 10.times do |x|
   Post.create(title: "Title #{x}", body: "Body #{x} words go here", user_id: User.first.id)
